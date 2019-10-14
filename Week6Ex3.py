833c3eaf3b450223b07be3da8820fc77c34df7f1
Anson Call <44686717+ansoncall@users.noreply.github.com>
2019-10-14 13:14:47 -0700

    Simplify and shorten code

--- Week6Ex3.py
+++ Week6Ex3.py
@@ -2,13 +2,8 @@
 
 # This script takes a user-inputted sentence, removes the spaces, changes all chars to lowercase, and counts the number of chars.
 
-# Get user input
-sentence = input('Please enter a sentence: ')
-
-# Remove spaces
-nospace = sentence.replace(' ', '')
-print(nospace)
-chars = nospace.len()
+# Get user input, change to lowercase, remove spaces
+sentence = input('Please enter a sentence: ').lower().replace(' ','')
 
 # Count chars and return
-print('The number of characters (excluding spaces) in the sentence is ', chars)
+print('The number of characters (excluding spaces) in the sentence is ', len(sentence))
