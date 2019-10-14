87e517fb6e38b00c2037fdbbae20d94a0cbc4e01
Anson Call <44686717+ansoncall@users.noreply.github.com>
2019-10-14 12:53:57 -0700

    Create Week6Ex2.py

---
+++ Week6Ex2.py
@@ -0,0 +1,7 @@
+#! usr/env/bin python3
+
+# This script counts the number of 2s in a hard-coded string and returns the value.
+
+num = '112345678911234566'
+twos = num.count('2')
+print('The number of 2s is ', twos)
